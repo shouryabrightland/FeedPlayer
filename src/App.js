2322bb9297a150fdafca2428e03d18c7eafca26b
shouryabrightland <shourya.brightland@gmail.com>
2026-04-10 19:42:33 +0530

    first layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import Playlist from './components/playlist/playlist';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+import Player from './components/player/player';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Player/>
+      <div className='content'>
+        <Playlist />
+        <Footer/>
+      </div>
+
+      
+      <Navbar/>
     </div>
   );
+
 }
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/components/playlist/list.js
@@ -0,0 +1,49 @@
+import "./list.css"
+export default function List(){
+    return (
+        <div className="list">
+            <div className="item">
+                <div className="side">
+                    <img src="logo192.png"/>
+                </div>
+                <div className="header">
+                    <div className="info">
+                        <div className="title">ABC</div>
+                        <div className="desc">Lorem, ipsum dolor sit amet consectetur</div>
+                    </div>
+                    <div className="options">
+                        <svg role="img" className="icon" viewBox="0 0 24 24"><path d="M10.5 4.5a1.5 1.5 0 1 0 3 0 1.5 1.5 0 0 0-3 0m0 15a1.5 1.5 0 1 0 3 0 1.5 1.5 0 0 0-3 0m0-7.5a1.5 1.5 0 1 0 3 0 1.5 1.5 0 0 0-3 0"></path></svg>
+                    </div>
+                </div>
+            </div>
+            <div className="item">
+                <div className="side">
+                    <img src="logo192.png"/>
+                </div>
+                <div className="header">
+                    <div className="info">
+                        <div className="title">ABC</div>
+                        <div className="desc">Lorem, ipsum dolor sit amet consectetur</div>
+                    </div>
+                    <div className="options">
+                        <svg role="img" className="icon" viewBox="0 0 24 24"><path d="M10.5 4.5a1.5 1.5 0 1 0 3 0 1.5 1.5 0 0 0-3 0m0 15a1.5 1.5 0 1 0 3 0 1.5 1.5 0 0 0-3 0m0-7.5a1.5 1.5 0 1 0 3 0 1.5 1.5 0 0 0-3 0"></path></svg>
+                    </div>
+                </div>
+            </div>
+            <div className="item">
+                <div className="side">
+                    <img src="logo192.png"/>
+                </div>
+                <div className="header">
+                    <div className="info">
+                        <div className="title">ABC</div>
+                        <div className="desc">Lorem, ipsum dolor sit amet consectetur</div>
+                    </div>
+                    <div className="options">
+                        <svg role="img" className="icon" viewBox="0 0 24 24"><path d="M10.5 4.5a1.5 1.5 0 1 0 3 0 1.5 1.5 0 0 0-3 0m0 15a1.5 1.5 0 1 0 3 0 1.5 1.5 0 0 0-3 0m0-7.5a1.5 1.5 0 1 0 3 0 1.5 1.5 0 0 0-3 0"></path></svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
